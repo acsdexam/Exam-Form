c3b90d51894d54e338724ce3a13e521a4ffd1557
acsdexam <91918408+acsdexam@users.noreply.github.com>
2024-04-20 17:20:06 -0400

    Update ExamForm.js Min date date.now()

--- exams-form/src/components/ExamForm.js
+++ exams-form/src/components/ExamForm.js
@@ -237,7 +237,7 @@ const ExamForm = () => {
                       <Input
                         type="date"
                         value={Inputs.examInfo[index].examDate}
-                        min={now.getDate()}
+                        min=Date(Date.now())
                        // min={Date.now()}
                        // min="2024-05-01"
                         max="2030-12-31"
