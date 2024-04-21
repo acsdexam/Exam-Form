8636b1bf7c518da568c8083ee81e119e08c58132
acsdexam <91918408+acsdexam@users.noreply.github.com>
2024-04-21 10:42:36 -0400

    Update ExamForm.js exp 5

--- exams-form/src/components/ExamForm.js
+++ exams-form/src/components/ExamForm.js
@@ -239,7 +239,7 @@ const ExamForm = () => {
                         value={Inputs.examInfo[index].examDate}
     //                    min={Date.now()}
      //                   min="2024-05-01"
-                        min = new Date(Date.now()).toString() 
+                        min = {new Date(Date.now()).toString()} 
 //                        min = {today}
                         max="2030-12-31"
                         onChange={(ev) => handleDateUpdate(ev, index)}
